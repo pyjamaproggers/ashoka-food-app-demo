2f79da0d571b0b3d22437e02a6e09d60c8a15674
zahaanshapoorjee <zahaan@shapoorjee.com>
2023-03-26 02:12:18 +0530

    Home Screen Template Complete

--- screens/RestaurantCards.js
+++ screens/RestaurantCards.js
@@ -3,12 +3,13 @@ import React from 'react'
 
 const RestaurantCards = ({name, image}) => {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity className="border-4 border-gray-300 mb-2 rounded-xl mx-4 ">
         <Image style={{
             alignSelf: 'center',
             height: 150,
             width: 150,
             borderWidth: 1,
+            marginTop: 10
         }} source= {image}/>
       <Text className="font-semibold text-xl p-3 font-mono">{name}</Text>
     </TouchableOpacity>
