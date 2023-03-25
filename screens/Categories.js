bcf57980153772f53987ef38903db75b0d222b2b
zahaanshapoorjee <zahaan@shapoorjee.com>
2023-03-26 01:15:32 +0530

    Featured Restaurants Added

--- screens/Categories.js
+++ screens/Categories.js
@@ -2,18 +2,25 @@ import { ScrollView, Text, View } from 'react-native'
 import React from 'react'
 import CategoryCard from './CategoryCard'
 import THC from '../assets/THC.jpg'
+import SubwayIcon from '../assets/subwayicon.png'
+import ChicagoPizzaIcon from '../assets/chicagopizzaicon.jpg'
+import Dosai from '../assets/dosai.jpg'
+import Dhaba from '../assets/dhaba.png'
+
 const Categories = () => {
   return (
     <ScrollView
     contentContainerStyle={{
         paddingHorizontal:15,
         paddingTop:10,
-    }}>
+    }}
+    horizontal
+    showsHorizontalScrollIndicator={false}>
       <CategoryCard image={THC} name = "The Hunger Cycle"/>
-      <CategoryCard image={THC} name = "Chicago Pizza"/>
-      <CategoryCard image={THC} name = "Roti Boti"/>
-      <CategoryCard image={THC} name = "Subway"/>
-      <CategoryCard image={THC} name = "Rassananda"/>
+      <CategoryCard image={SubwayIcon} name = "Subway"/>
+      <CategoryCard image={ChicagoPizzaIcon} name = "Chicago Pizza"/>
+      <CategoryCard image={Dhaba} name = "Dhaba"/>
+      <CategoryCard image={Dosai} name = "Dosai"/>
     </ScrollView>
   )
 }
--- screens/CategoryCard.js
+++ screens/CategoryCard.js
@@ -1,11 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 
 const CategoryCard = ({image, name}) => {
   return (
-    <View>
+    <TouchableOpacity>
+        <Image source={image} className="h-20 w-20 rounded bg-white"/>
       <Text>{name}</Text>
-    </View>
+    </TouchableOpacity>
   )
 }
 
