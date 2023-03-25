6d8bf5f1cfb050c5b547c4c90094dc481361f981
zahaanshapoorjee <zahaan@shapoorjee.com>
2023-03-25 23:17:02 +0530

    Working on HomeScreen

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
+import AshokaLogo from './assets/ASHOKAWHITELOGO.png'
 
 const Stack = createNativeStackNavigator();
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, TextInput } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import React, { useLayoutEffect } from 'react'
-import { StyleSheet } from 'react-native'
-import { StatusBar } from 'expo-status-bar';
 import { useNavigation } from '@react-navigation/native';
+import AshokaLogo from '../assets/ASHOKAWHITELOGO.png';
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+} from "react-native-heroicons/outline" 
+import {AdjustmentsIcon} from "react-native-heroicons/outline"
 const HomeScreen = () => {
 
 const navigation = useNavigation();
@@ -14,10 +19,29 @@ useLayoutEffect(()=>{
     })
 }, [])
   return (
-    <SafeAreaView>
-      <Text>Home Screen</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <SafeAreaView className="bg-white pt-5">
+      <Text className="text-red-500">
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image 
+        source ={AshokaLogo} className="h-7 w-7 bg-gray-300 p-4 rounded-full"/>
+
+            <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+            <Text className="font-bold text-xl">Current Location
+            <ChevronDownIcon size={20} color="black"/>
+            </Text>
+            </View>
+
+        <UserIcon size={35} color = "#00CCBB" />
+      </View>
+      <View>
+        <View>
+
+        </View>
+        
+      </View>
+    </Text>
+  </SafeAreaView>
   )
 }
 export default HomeScreen
