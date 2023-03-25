718543d78dc8109c12a6b96e138a78b37dd9c2fb
zahaanshapoorjee <zahaan@shapoorjee.com>
2023-03-25 23:51:34 +0530

    HomeScreen Top Done

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,9 +6,10 @@ import AshokaLogo from '../assets/ASHOKAWHITELOGO.png';
 import {
   UserIcon,
   ChevronDownIcon,
-  SearchIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
 } from "react-native-heroicons/outline" 
-import {AdjustmentsIcon} from "react-native-heroicons/outline"
+
 const HomeScreen = () => {
 
 const navigation = useNavigation();
@@ -20,27 +21,31 @@ useLayoutEffect(()=>{
 }, [])
   return (
     <SafeAreaView className="bg-white pt-5">
-      <Text className="text-red-500">
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
-        <Image 
-        source ={AshokaLogo} className="h-7 w-7 bg-gray-300 p-4 rounded-full"/>
-
-            <View className="flex-1">
-            <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-            <Text className="font-bold text-xl">Current Location
-            <ChevronDownIcon size={20} color="black"/>
-            </Text>
-            </View>
-
-        <UserIcon size={35} color = "#00CCBB" />
-      </View>
-      <View>
-        <View>
-
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 ">
+        <Image
+          source={AshokaLogo}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
         </View>
-        
+        <UserIcon size={35} color="#00CCBB" />
       </View>
-    </Text>
+      {/* search */}
+      <View className="flex-row item-center space-x-2 pb-2 mx-4">
+        <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="gray" size={20} />
+          <TextInput
+            placeholder="What would you like to order?"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
   </SafeAreaView>
   )
 }
